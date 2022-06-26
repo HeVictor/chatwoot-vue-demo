27250317d4fdac4ea370a776689b4f50a199c370
HeVictor <victorjunlei@yahoo.com>
2022-06-26 18:56:23 +1000

    adds ChatwootWidget into the home page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
     <v-app>
         <app-navigation></app-navigation>
-
         <v-content transition="slide-x-transition">
             <router-view></router-view>
         </v-content>
+        <chatwoot-widget />
     </v-app>
 </template>
 
 <script>
 import AppNavigation from '@/components/AppNavigation';
+import ChatwootWidget from './components/ChatwootWidget.vue';
 
 export default {
     name: 'App',
     components: {
         AppNavigation,
+        ChatwootWidget,
     },
 };
 </script>
